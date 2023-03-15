2bd49aeff3460a337ba717d364149956433a01a1
Billy <billy@pc186.wireless50.tku.edu.tw>
2023-03-15 21:21:07 +0800

    implement route /card2_node_xx on both local and render (cloud) server

--- src/App_96.js
+++ src/App_96.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import BlogsStaticPage_96 from "./pages/BlogsStaticPage_96.js";
 import BlogsLocalJsonPage_96 from "./pages/BlogsLocalJsonPage_96.js";
+import BlogsNodeServerPage_96 from "./pages/BlogsNodeServerPage_96.js";
 import HomePage_96 from "./pages/HomePage_96";
 
 function App() {
@@ -10,7 +11,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/card_96" element={<BlogsStaticPage_96 />}></Route>
-        <Route path="/card2_local_96" element={<BlogsLocalJsonPage_96 />}></Route>
+        <Route
+          path="/card2_local_96"
+          element={<BlogsLocalJsonPage_96 />}
+        ></Route>
+        <Route
+          path="/card2_node_96"
+          element={<BlogsNodeServerPage_96 />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
